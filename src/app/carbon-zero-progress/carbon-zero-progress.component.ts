f52e34e77e60e605aa9ef6ef0dfdeb13dead5875
hamidrezakarami <hamidrezakarami16@gmail.com>
2023-06-25 14:24:19 +0330

    refactor:extra lines removed

--- src/app/carbon-zero-progress/carbon-zero-progress.component.ts
+++ src/app/carbon-zero-progress/carbon-zero-progress.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
-
-import { IXYChartSettings } from '@amcharts/amcharts5/xy';
-
-import * as am5 from '@amcharts/amcharts5';
 import {
   IAxisLabel,
   ICarbonZeroProgress,
   IEditAxisLabel,
   IEditCarbonZeroProgress,
 } from './interfaces';
+import {
+  CHART_AXIS_LABELS,
+  CHART_DATA,
+  CHART_SETTINGS,
+  CHART_TARGET_SETTINGS,
+  CHART_VALUE_AXIS_SETTINGS,
+} from './configs';
 
 @Component({
   selector: 'app-carbon-zero-progress',
@@ -16,168 +19,13 @@ import {
   styleUrls: ['./carbon-zero-progress.component.scss'],
 })
 export class CarbonZeroProgressComponent {
-  xyChart1: IXYChartSettings = {
-    panX: false,
-    panY: false,
-    wheelX: 'none',
-    wheelY: 'none',
-    paddingRight: 30,
-  };
-  xyChart2: IXYChartSettings = {
-    panX: true,
-    panY: true,
-    wheelX: 'none',
-    wheelY: 'none',
-    paddingRight: 300,
-  };
-  test = {
-    dy: -38,
-    centerY: 200,
-    centerX: 50,
-    populateText: true,
-    paddingTop: 5,
-    paddingRight: 5,
-    paddingBottom: 5,
-    paddingLeft: 5,
-  };
+  valueAxis = CHART_VALUE_AXIS_SETTINGS;
+  chartSettings = CHART_SETTINGS;
+  chartData = CHART_DATA;
+  axisLabels = CHART_AXIS_LABELS;
+  targetSettings = CHART_TARGET_SETTINGS;
 
-  chartData: Array<ICarbonZeroProgress> = [
-    {
-      category: '15',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0xc6251a),
-      },
-    },
-    {
-      category: '14',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0xfcc034),
-      },
-    },
-    {
-      category: '13',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0xc6251a),
-      },
-    },
-    {
-      category: '12',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0xc6251a),
-      },
-    },
-    {
-      category: '11',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0xc6251a),
-      },
-    },
-    {
-      category: '10',
-      value: 100,
-      currentBullet: true,
-      columnSettings: {
-        fill: am5.color(0xfcc034),
-      },
-    },
-    {
-      category: '9',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0xfcc034),
-      },
-    },
-    {
-      category: '8',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0xfcc034),
-      },
-    },
-    {
-      category: '7',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0xfcc034),
-      },
-    },
-    {
-      category: '6',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0xfcc034),
-      },
-    },
-    {
-      category: '5',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0x6bc352),
-      },
-    },
-    {
-      category: '4',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0x6bc352),
-      },
-    },
-    {
-      category: '3',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0x6bc352),
-      },
-    },
-    {
-      category: '2',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0x6bc352),
-      },
-    },
-    {
-      category: '1',
-      value: 100,
-      columnSettings: {
-        fill: am5.color(0x6bc352),
-      },
-    },
-    {
-      category: '0',
-      value: 100,
-      targetBullet: true,
-      columnSettings: {
-        fill: am5.color(0xffffff),
-      },
-    },
-  ];
-
-  axisLabels: Array<IAxisLabel> = [
-    { category: '15', text: '20+' },
-    { category: '10', text: '10' },
-    { category: '5', text: '5' },
-  ];
-
-  constructor() {
-    // setTimeout(() => {
-    //   this.test = {
-    //     dy: -38,
-    //     centerY: 400,
-    //     centerX: 50,
-    //     populateText: true,
-    //     paddingTop: 50,
-    //     paddingRight: 5,
-    //     paddingBottom: 5,
-    //     paddingLeft: 5,
-    //   };
-    // }, 5000);
-  }
+  constructor() {}
 
   onDeleteChartDataItem(item: ICarbonZeroProgress) {
     this.chartData = this.chartData.filter((el) => el !== item);
@@ -203,7 +51,7 @@ export class CarbonZeroProgressComponent {
   }
 
   onAddChartDataItem(item: ICarbonZeroProgress) {
-    this.chartData = [...this.chartData, item];
+    this.chartData = [item, ...this.chartData];
   }
 
   onAddAxisLabelItem(item: IAxisLabel) {
